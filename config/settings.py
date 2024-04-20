d51576eb812b17842dce39159d03a13a313715e4
WebDeveloperM <webdevelopermm1514@gmail.com>
2024-04-20 22:59:18 +0500

    deploy nasib

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', "False").lower() == 'true'
-
+# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 # Application definition
@@ -107,9 +107,7 @@ DATABASES = {
 database_url = os.environ.get('DATABASE_URL')
 
 DATABASES['default'] = dj_database_url.parse(database_url)
-
-#
-# 'postgres://mahalla_database_user:kMkHRtn0wjwmKwVFDvQan1XOzGPEdWV6@dpg-coho8sgl5elc73cra4n0-a.oregon-postgres.render.com/mahalla_database'
+# DATABASES['default'] = dj_database_url.parse('postgres://mahalla_database_user:kMkHRtn0wjwmKwVFDvQan1XOzGPEdWV6@dpg-coho8sgl5elc73cra4n0-a.oregon-postgres.render.com/mahalla_database')
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -144,7 +142,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'files/assets')
-
+#
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
