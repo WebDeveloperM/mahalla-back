a173dc163f3d69945a309ab924f4a7ef1def2d6d
WebDeveloperM <webdevelopermm1514@gmail.com>
2024-04-21 01:11:58 +0500

    white list

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:8000',
+    'https://mahalla-online.onrender.com/',
+    'mahalla-online.onrender.com/'
 )
 
 ROOT_URLCONF = 'config.urls'
